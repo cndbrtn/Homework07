fa32fa004ad03d2e71b1178dd7625b7dbf0831d8
Cindy Burton <cindy.brton@gmail.com>
2019-12-11 15:57:39 -0700

    added links to the nav

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,27 @@ $(document).on("click touch", "#contact", function () {
     }
 
 })
+
+$(".hover").hover(
+    function () {
+        console.log("hover test", $(this).attr("data-value"))
+        if ($(this).attr("data-value") === "folio1") {
+            $("#hover-text").text("This is some new text for blah1!")
+        }
+        if ($(this).attr("data-value") === "folio2") {
+            $("#hover-text").text("This is some new text for blah2!")
+        }
+        if ($(this).attr("data-value") === "folio3") {
+            $("#hover-text").text("This is some new text for blah3!")
+        }
+        if ($(this).attr("data-value") === "folio4") {
+            $("#hover-text").text("This is some new text for blah4!")
+        }
+        if ($(this).attr("data-value") === "folio5") {
+            $("#hover-text").text("This is some new text for blah5!")
+        }
+    }, function () {
+        $("#hover-text").text("Hover over an image for more information")
+    });
+
+
