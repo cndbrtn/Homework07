9de7f6612cef015d0e652982763f765cf27d689a
Cindy Burton <cindy.brton@gmail.com>
2019-12-11 00:42:07 -0700

    ok wait I gave the li's ids and made only contact clickable

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 $(".container").show(500, "linear")
 $(".container").attr("style", "display: flex;")
 
-$(document).on("click touch", "li", function () {
+$(document).on("click touch", "#contact", function () {
     console.log("hit")
     console.log($(".modal").attr("style"))
+
     if ($(".modal").attr("style") === "display: block;") {
         $(".modal").hide();
     }
