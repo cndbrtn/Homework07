f89c19120746b426e1997c3af5d492cc389de659
Cindy Burton <cindy.brton@gmail.com>
2019-12-11 00:39:03 -0700

    I'm going to bed but I got the modal working at computer screen sizes

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,14 @@
 $(".container").show(500, "linear")
 $(".container").attr("style", "display: flex;")
+
+$(document).on("click touch", "li", function () {
+    console.log("hit")
+    console.log($(".modal").attr("style"))
+    if ($(".modal").attr("style") === "display: block;") {
+        $(".modal").hide();
+    }
+    else {
+        $(".modal").show()
+    }
+
+})
